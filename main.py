9756677abbb543331c7f5cae998d3b1b592b2096
yaroslav8765 <pechorkin2014@gmail.com>
2025-03-30 11:48:36 +0300

    Added get goods detailed info. Tested all functionality and everything should work fine. Now need to make fronfend and unit tests

--- main.py
+++ main.py
@@ -14,14 +14,6 @@ try:
 except Exception as e:
     print(f"❌ Ошибка подключения: {e}")
 
-
-
-from routers.auth import check_if_user_enter_email_or_phone_num
-@app.get("/{test_login}")
-async def test(test_login:str):
-    return check_if_user_enter_email_or_phone_num(test_login)
-    return{"message": "Hello World"}
-
 app.include_router(auth.router)
 app.include_router(email_verification.router)
 app.include_router(admin.router)

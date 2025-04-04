a36ea0f4c54e195b74a38ba9dd7fafb98c12f588
yaroslav8765 <pechorkin2014@gmail.com>
2025-04-04 22:17:29 +0300

    imporved previous unit tests and made new

--- unit_tests/test_users.py
+++ unit_tests/test_users.py
@@ -9,6 +9,8 @@ from fastapi.testclient import TestClient
 from fastapi import status
 from ..models import Smartphones, Laptops, Orders, OrderItem, Basket, Goods, Users
 
+
+
 SQLALCHEMY_DATABESE_URL = "sqlite:///./testdb.db"
 
 engine = create_engine(
@@ -31,9 +33,9 @@ def override_get_db():
 def override_get_current_user():
     return {"username": "pechorkin2014@gmail.com", "id": 1, "role": "user"}
 
-    
-app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
+app.dependency_overrides[get_db] = override_get_db
+
 
 client = TestClient(app)
 
@@ -97,6 +99,48 @@ def test_user():
         connection.execute(text("DELETE FROM users;"))
         connection.commit()             
 
+@pytest.fixture
+def test_unauthorized_user():
+    user = Users(
+        id = 1,
+        email = "pechorkin2014@gmail.com",
+        first_name = "Yaroslav",
+        last_name = "Pechorkin",
+        hashed_password = "35bu35jv5v7jv567jv6347j44j",
+        is_active = True,
+        role = "unknown",
+
+    )
+
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM users;"))
+        connection.commit()        
+
+@pytest.fixture
+def test_admin_user():
+    user = Users(
+        id = 1,
+        email = "pechorkin2014@gmail.com",
+        first_name = "Yaroslav",
+        last_name = "Pechorkin",
+        hashed_password = "35bu35jv5v7jv567jv6347j44j",
+        is_active = True,
+        role = "admin",
+
+    )
+
+    db = TestingSessionLocal()
+    db.add(user)
+    db.commit()
+    yield user
+    with engine.connect() as connection:
+        connection.execute(text("DELETE FROM users;"))
+        connection.commit()        
+
 
 #############################################################################
 #                                                                           #
@@ -121,24 +165,37 @@ def test_add_to_the_basket(test_basket, test_good):
         "goods_id": 2,
         "quantity": 5,
         }
-    response = client.post("/user/add-to-basket", json = request_data)
-    assert response.status_code == status.HTTP_201_CREATED
+    try:
+        response = client.post("/user/add-to-basket", json = request_data)
+        assert response.status_code == status.HTTP_201_CREATED
 
-    user = {"username": "pechorkin2014@gmail.com", "id": 1, "role": "user"}
-    db = TestingSessionLocal()
-    model = db.query(Basket).filter(Basket.user_id == user.get("id")).filter(Basket.goods_id == request_data.get("goods_id")).first()
-    assert model.user_id == user.get("id")
-    assert model.quantity == request_data.get("quantity") + (test_basket.quantity - request_data.get("quantity"))
-    assert model.goods_id == request_data.get("goods_id")
-    assert model.price_for_the_one == test_good.price
+        user = {"username": "pechorkin2014@gmail.com", "id": 1, "role": "user"}
+        db = TestingSessionLocal()
+        model = db.query(Basket).filter(Basket.user_id == user.get("id")).filter(Basket.goods_id == request_data.get("goods_id")).first()
+        assert model.user_id == user.get("id")
+        assert model.quantity == request_data.get("quantity") + (test_basket.quantity - request_data.get("quantity"))
+        assert model.goods_id == request_data.get("goods_id")
+        assert model.price_for_the_one == test_good.price
+    finally:
+        db = TestingSessionLocal()
+        db.query(Basket).delete()
+        db.commit()
+        db.close()
 
 def test_add_to_the_basket_unexisting_item(test_good):
     request_data = {
         "goods_id": 999,
         "quantity": 5,
         }
-    response = client.post("/user/add-to-basket", json = request_data)
-    assert response.status_code == status.HTTP_404_NOT_FOUND
+    try:
+        response = client.post("/user/add-to-basket", json = request_data)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.json() == {"detail": "No goods with such ID"}
+    finally:
+        db = TestingSessionLocal()
+        db.query(Basket).delete()
+        db.commit()
+        db.close()
 
 
 def test_add_to_the_basket_more_than_exist(test_good):
@@ -146,8 +203,37 @@ def test_add_to_the_basket_more_than_exist(test_good):
         "goods_id": 2,
         "quantity": 500,
         }
-    response = client.post("/user/add-to-basket", json = request_data)
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    try:
+        response = client.post("/user/add-to-basket", json = request_data)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {"detail": "We don`t have enought goods. There is only 23"}
+    finally:
+        db = TestingSessionLocal()
+        db.query(Basket).delete()
+        db.commit()
+        db.close()
+
+def test_add_to_the_basket_user_is_none(test_good):
+    request_data = {
+        "goods_id": 2,
+        "quantity": 5,
+    }
+
+    app.dependency_overrides[get_current_user] = lambda: None
+
+    try:
+        response = client.post("/user/add-to-basket", json=request_data)
+        assert response.status_code == status.HTTP_201_CREATED
+        assert response.json() == {
+            "message": "Sorry, but at this moment if you want to add good to the basket you need to create accout first"
+        }
+    finally:
+        db = TestingSessionLocal()
+        db.query(Basket).delete()
+        db.commit()
+        db.close()
+        app.dependency_overrides[get_current_user] = override_get_current_user
+
 
 
 #############################################################################
@@ -155,6 +241,7 @@ def test_add_to_the_basket_more_than_exist(test_good):
 #                           Edit users basket                               #
 #                                                                           #
 #############################################################################
+
 
 def test_edit_basket(test_basket, test_good):
     request_data = {
@@ -214,22 +301,52 @@ def test_create_order(test_basket, test_good, test_user):
         "reciever_name": "Tohru",
         "shipping_adress": "Miss Kobayashi's Home"
     }
-    db = TestingSessionLocal()
+    try:
+        db = TestingSessionLocal()
+        goods_in_basket = db.query(Basket).filter(Basket.user_id == test_user.id).count()
 
-    goods_in_basket = db.query(Basket).filter(Basket.user_id == test_user.id).count()
+        response = client.post("/user/order", json=request_data)
+        
+        assert response.status_code == status.HTTP_200_OK
+        
+        created_order = db.query(Orders).filter(Orders.user_id == test_user.id).first()
+        assert created_order is not None
+        assert created_order.reciever_name == request_data["reciever_name"]
+        assert created_order.shipping_adress == request_data["shipping_adress"]
+        assert created_order.total_price > 0
 
-    response = client.post("/user/order", json=request_data)
-    
-    assert response.status_code == status.HTTP_200_OK
-    
-    created_order = db.query(Orders).filter(Orders.user_id == test_user.id).first()
-    assert created_order is not None
-    assert created_order.reciever_name == request_data["reciever_name"]
-    assert created_order.shipping_adress == request_data["shipping_adress"]
-    assert created_order.total_price > 0
+        order_items = db.query(OrderItem).filter(OrderItem.order_id == created_order.order_number).all()
+        assert len(order_items) == goods_in_basket
+        
+        basket_empty = db.query(Basket).filter(Basket.user_id == test_user.id).count() == 0
+        assert basket_empty
+    finally:
+        db = TestingSessionLocal()
+        db.query(OrderItem).delete()
+        db.query(Orders).delete()
+        db.commit()
+        db.close()
 
-    order_items = db.query(OrderItem).filter(OrderItem.order_id == created_order.order_number).all()
-    assert len(order_items) == goods_in_basket
-    
-    basket_empty = db.query(Basket).filter(Basket.user_id == test_user.id).count() == 0
-    assert basket_empty
+
+def test_create_order_no_user():
+    request_data = {
+        "reciever_name": "Tohru",
+        "shipping_adress": "Miss Kobayashi's Home"
+    }
+    try:
+        app.dependency_overrides[get_current_user] = lambda: None
+
+        response = client.post("/user/order", json=request_data)
+        
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {
+            "message": "Sorry, but at this moment if you want to add good to the basket you need to create accout first"
+        }
+
+        app.dependency_overrides[get_current_user] = override_get_current_user
+    finally:
+        db = TestingSessionLocal()
+        db.query(OrderItem).delete()
+        db.query(Orders).delete()
+        db.commit()
+        db.close()
